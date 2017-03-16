c46b6fd6d1bd5ea4b4553b0b5e55fac1abb99a9d
LeeJungwoo <LeeJungwoo@DESKTOP-17BS9SS>
2017-03-16 14:20:20 +0900

    실습과제 3

---
+++ JavaPractice/src/org/dimigo/basic/Loop.java
@@ -0,0 +1,71 @@
+/**
+ * 
+ */
+package org.dimigo.basic;
+
+import java.util.*;
+
+/**
+ * <pre>
+ * org.dimigo.basic
+ *   |_ Loop
+ *
+ * 1. 개요 : 초간단 게임프로그램
+ * 2. 작성일 : 2017. 3. 16.
+ * </pre>
+ *
+ * @author		: LeeJungwoo
+ * @version		: 1.0
+ */
+public class Loop {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Random random = new Random();
+		
+		Scanner scanner = new Scanner(System.in);
+		int choice;
+		int sum = 100;
+		int i = 10;
+		String[] character = {"마법사", "영주", "기사", "농민"};
+
+		
+		
+		do {
+			System.out.println("--------------------");
+			System.out.println("<< 게임 메뉴 >>");
+			System.out.println("1. 공격력 증가");
+			System.out.println("2. 공격력 감소");
+			System.out.println("3. 캐릭터 설정");
+			System.out.println("9. 종료");
+			System.out.println("--------------------");
+			System.out.println("메뉴 입력 => ");
+			choice = scanner.nextInt();
+			switch (choice) {
+			case 1 :
+				sum += i;
+				System.out.println("공격력이 증가되었습니다. 현재 공격력 : "+sum);
+				break;
+			case 2 : 
+				sum -= i;
+				System.out.println("공격력이 감소되었습니다. 현재 공격력 : "+sum);
+				break;
+			case 3 :
+				int a = random.nextInt(4);
+				System.out.println(character[a]+"(으)로 설정되었습니다.");
+				break;
+			case 9:
+				System.out.println("이제 공부하세요!");
+				break;	
+			default :
+				System.out.println("없는 메뉴입니다!!");
+				scanner.close();
+			}
+		} while (choice != 9);
+		
+	}
+
+}
