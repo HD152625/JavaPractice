c99715d2dec932764a0d193ee207d9c87ebeb001
LeeJungwoo <LeeJungwoo@DESKTOP-17BS9SS>
2017-03-28 10:22:45 +0900

    실습과제 3

---
+++ JavaPractice/src/org/dimigo/oop/Car3.java
@@ -0,0 +1,84 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ *   |_ Car
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2017. 3. 23.
+ * </pre>
+ *
+ * @author		: LeeJungwoo
+ * @version		: 1.0
+ */	
+public class Car3 {
+
+	   private String company = "현대자동차";
+	   private String model = "제네시스";
+	   private String color = "검정색";
+	   private int maxSpeed = 225;
+	   private int price = 50000000; 
+	   
+	   public Car3() {
+	  
+	   }
+	   public Car3(String company, String model, String color, int maxSpeed, int price){
+		   this.company = company;
+		   this.model = model;
+		   this.color = color;
+		   this.maxSpeed = maxSpeed;
+		   this.price = price;
+	   }
+	   public Car3(String company, String model, String color, int maxSpeed){
+		   this(company, model, color, 225, 0);
+	   }
+	   
+	   public Car3(String company, String model, String color){
+		   this(company, model, color, 0, 0);
+	   }
+	   
+
+	   public void setCompany(String newCompany) {
+	      company = newCompany;
+	   }
+	   public String getCompany() {
+	      return company;
+	   }
+	   
+	   public void setModel(String newModel) {
+	      model = newModel;
+	   }
+	   public String getModel() {
+	      return model;
+	   }
+	   
+	   public void setColor(String newColor) {
+	      color = newColor;
+	   }
+	   public String getColor() {
+	      return color;
+	   }
+	   
+	   public void setmaxSpeed(int newmaxSpeed) {
+	      maxSpeed = newmaxSpeed;
+	   }
+	   public int getmaxSpeed() {
+	      return maxSpeed;
+	   }
+	   
+	   public void setPrice(int newPrice) {
+	      price = newPrice;
+	   }
+	   public int getPrice() {
+	      return price;
+	   }
+	/**
+	 * @param string
+	 */
+
+		
+	}
