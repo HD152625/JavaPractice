81c3af9b79b47d59f0e867006a2b3a234a094a43
LeeJungwoo <LeeJungwoo@DESKTOP-17BS9SS>
2017-05-16 14:44:53 +0900

    실습

---
+++ JavaPractice/src/org/dimigo/abstractclass/SmartPhoneTest.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package org.dimigo.abstractclass;
+
+/**
+ * <pre>
+ * org.dimigo.abstractclass
+ *   |_ SmartPhoneTest
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2017. 5. 16.
+ * </pre>
+ *
+ * @author		: LeeJungwoo
+ * @version		: 1.0
+ */
+public class SmartPhoneTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
