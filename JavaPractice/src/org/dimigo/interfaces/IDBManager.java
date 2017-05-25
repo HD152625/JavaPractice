4347e0612f9a81abc7b84ee1ab9727ab24a2aadb
LeeJungwoo <LeeJungwoo@DESKTOP-17BS9SS>
2017-05-25 14:44:01 +0900

    데이터베이스

---
+++ JavaPractice/src/org/dimigo/interfaces/IDBManager.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package org.dimigo.interfaces;
+
+/**
+ * <pre>
+ * org.dimigo.interfaces
+ *   |_ IDBManager
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2017. 5. 25.
+ * </pre>
+ *
+ * @author		: LeeJungwoo
+ * @version		: 1.0
+ */
+public interface IDBManager {
+	String ORACLE_DATABASE = "ORACLE";
+	String SYBASE_DATABASE = "SYBASE";
+	
+	void insert();
+	void search();
+	void update();
+	void delete();
+	
+	static IDBManager getDBObject(String database){
+		if (ORACLE_DATABASE.equals(database)){
+			return new OracleDB();
+		}
+		else return new SybaseDB();
+		
+	}
+}
